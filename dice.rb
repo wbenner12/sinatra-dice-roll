6f04306871ea5f8cf064b46023b88ae46c448194
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-17 05:11:17 +0000

    committed all over it

--- dice.rb
+++ dice.rb
@@ -21,41 +21,61 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
 	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six, { :layout => :wrapper })
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
 	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_ten, { :layout => :wrapper })
+	erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
 	
-	@outcome = "You rolled a #{die}."
-	
-	erb(:one_twenty, { :layout => :wrapper })
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
 	
-	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
-	
-	erb(:five_four, { :layout => :wrapper })
+	erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
