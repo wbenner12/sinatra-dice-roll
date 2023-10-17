4188e563ad2c0f53333ee6e1b030c780780df1b8
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-17 04:49:38 +0000

    but I'm not a wrapper

--- dice.rb
+++ dice.rb
@@ -27,7 +27,7 @@ get("/dice/2/6") do
 	
 	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-	erb(:two_six)
+	erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do
@@ -37,7 +37,7 @@ get("/dice/2/10") do
 	
 	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-	erb(:two_ten)
+	erb(:two_ten, { :layout => :wrapper })
 end
 
 get("/dice/1/20") do
@@ -45,7 +45,7 @@ get("/dice/1/20") do
 	
 	@outcome = "You rolled a #{die}."
 	
-	erb(:one_twenty)
+	erb(:one_twenty, { :layout => :wrapper })
 end
 
 get("/dice/5/4") do
@@ -57,5 +57,5 @@ get("/dice/5/4") do
 	
 	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-	erb(:five_four)
+	erb(:five_four, { :layout => :wrapper })
 end
